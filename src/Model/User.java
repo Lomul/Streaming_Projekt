776f7a64eceadccd930387b63cfd29af2affe828
Malou <elmelund@hotmail.dk>
2019-12-02 15:06:01 +0100

    User class name,watched, saved.

--- src/Model/User.java
+++ src/Model/User.java
@@ -1,9 +1,26 @@
 package Model;
 
-import java.util.List;
+import java.util.HashMap;
+import java.util.*;
 
 public class User {
     String name;
     List<Watchable> saved;
     List<Watchable>  watched;
+
+    public User (String name){
+        this.name = name;
+        saved = new HashMap<Watchable>(); //vi skal bruge en key dunno
+        watched = new HashMap<Watchable>();
+    }
+    public void addSaved(Watchable watchable){
+        saved.put("",watchable);
+    }
+    public void addWatched(Watchable watchable){
+        watched.put("",watchable);
+    }
+
+    //Add to ”Saved”
+    //Add film and series to a list of “watched”
+    //Change Account
 }
