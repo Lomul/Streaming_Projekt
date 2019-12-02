911345a266484ca9bcd29c02f94bd1d7fcaaa450
Malou <elmelund@hotmail.dk>
2019-12-02 15:43:49 +0100

    Watchable

--- src/Model/Watchable.java
+++ src/Model/Watchable.java
@@ -1,10 +1,16 @@
 package Model;
 
+import java.util.List;
+
 public class Watchable {
     String title;
     double rating;
+    String year;
+    List<Watchable> genre;
 
-    public Watchable(String title, double rating)
+    public Watchable(String title, double rating, String year)
+                //laver vi en string af genre også ændre dem i konstruktor
+                //eller laver vi en liste af genre også smider det ind som parameter?
     {
         this.title = title;
         this.rating = rating;
